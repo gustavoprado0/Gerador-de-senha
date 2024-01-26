b9efa9fc198d4489037af86e01b713f7656c6ab5
gustavoprado0 <gustavoprd1@gmail.com>
2024-01-26 11:35:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function gerarSenha() {
+  // Caracteres permitidos na senha
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  // Comprimento da senha desejado
+  const comprimentoSenha = 12;
+
+  let senha = '';
+  for (let i = 0; i < comprimentoSenha; i++) {
+    const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres.charAt(indiceAleatorio);
+  }
+
+  // Exibe a senha gerada
+  document.getElementById('password').textContent = 'Senha: ' + senha;
+}
